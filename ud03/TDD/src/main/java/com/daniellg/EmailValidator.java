40bc8b55fcd65a1239a4e7bb0b9f1e0540b730c0
Dani-LG <dani.l.g333@gmail.com>
2025-02-06 12:54:23 +0100

    TDD version 1

---
+++ ud03/TDD/src/main/java/com/daniellg/EmailValidator.java
@@ -0,0 +1,15 @@
+package com.daniellg;
+
+public class EmailValidator {
+
+    public static boolean Validator(String email) {
+        boolean result = true;
+        try {
+           InternetAddress emailAddr = new InternetAddress(email);
+           emailAddr.validate();
+        } catch (AddressException ex) {
+           result = false;
+        }
+        return result;
+     }
+}
